d0383ee8026aaa89c582f74817ba5877d868d157
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-09-26 15:37:22 +0200

    Add Township model

---
+++ PizzaMario/Models/Township.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaMario.Models
+{
+    public class Township
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+}
