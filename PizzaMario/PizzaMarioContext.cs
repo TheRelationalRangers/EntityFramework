9c051635ad1cb6f70589404b1605ce96c72835c8
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-09-26 16:20:35 +0200

    Add Store and Deliveryrange dbset

--- PizzaMario/PizzaMarioContext.cs
+++ PizzaMario/PizzaMarioContext.cs
@@ -13,5 +13,7 @@ namespace PizzaMario
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Township> Townships { get; set; }
         public DbSet<Address> Addresses { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<DeliveryRange> DeliveryRanges { get; set; }
     }
 }
