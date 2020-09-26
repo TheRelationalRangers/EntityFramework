10b882eedceaa184f2ab4a13e80bfdda3127efa0
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-09-26 15:37:34 +0200

    Add Zipcode model

---
+++ PizzaMario/Models/Zipcode.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaMario.Models
+{
+    public class Zipcode
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+        public string Addition { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+
+        public int SeriesIndicationStart { get; set; }
+        public int SeriesIndicationEnd { get; set; }
+
+        public Township Township { get; set; }
+    }
+}
