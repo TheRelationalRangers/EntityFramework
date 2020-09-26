f877c6e7916068d408e6b8e3bd7f2d7c0bb1235d
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-09-26 15:37:11 +0200

    Add Customer model

---
+++ PizzaMario/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMario.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Addition { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public Zipcode Zipcode { get; set; }
+    }
+}
