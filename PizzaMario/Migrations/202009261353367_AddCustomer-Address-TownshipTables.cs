25895c60a99a05a522a3e8fd23099d313236ef58
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-09-26 15:57:11 +0200

    Add PizzaMarioContext

---
+++ PizzaMario/Migrations/202009261353367_AddCustomer-Address-TownshipTables.cs
@@ -0,0 +1,68 @@
+﻿namespace PizzaMario.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCustomerAddressTownshipTables : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Addresses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Street = c.String(),
+                        HouseNumber = c.Int(nullable: false),
+                        Addition = c.String(),
+                        ZipCode = c.String(),
+                        City = c.String(),
+                        SeriesIndicationStart = c.Int(nullable: false),
+                        SeriesIndicationEnd = c.Int(nullable: false),
+                        Township_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Townships", t => t.Township_Id)
+                .Index(t => t.Township_Id);
+            
+            CreateTable(
+                "dbo.Townships",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Code = c.Int(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Addition = c.String(),
+                        LastName = c.String(),
+                        PhoneNumber = c.String(),
+                        Mail = c.String(),
+                        Password = c.String(),
+                        ZipCode_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Addresses", t => t.ZipCode_Id)
+                .Index(t => t.ZipCode_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "ZipCode_Id", "dbo.Addresses");
+            DropForeignKey("dbo.Addresses", "Township_Id", "dbo.Townships");
+            DropIndex("dbo.Customers", new[] { "ZipCode_Id" });
+            DropIndex("dbo.Addresses", new[] { "Township_Id" });
+            DropTable("dbo.Customers");
+            DropTable("dbo.Townships");
+            DropTable("dbo.Addresses");
+        }
+    }
+}
--- PizzaMario/PizzaMarioContext.cs
+++ PizzaMario/PizzaMarioContext.cs
@@ -5,6 +5,11 @@ namespace PizzaMario
 {
     public class PizzaMarioContext : DbContext
     {
+        public PizzaMarioContext()
+            : base("name=PizzaMario")
+        {
+        }
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Township> Townships { get; set; }
         public DbSet<Address> Addresses { get; set; }
